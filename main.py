42d0e4af1f40a4355ca227787ca57d63927fe0ba
JavierDioses84 <85272734+JavierDioses84@users.noreply.github.com>
2023-04-21 18:18:08 -0500

    commit 618

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ a = 25
 
 b = 35
 
+print("Verificando cambios 32")
+
+print("Verificando cambios 41")
+
+print("Verificando cambios 5")
+
+print("Verificando cambios 6")
+
 print(a + b)
 
 print(a//b)
